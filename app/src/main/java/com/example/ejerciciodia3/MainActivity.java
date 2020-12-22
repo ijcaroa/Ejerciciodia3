18261099b133f9adcd9130a725dae6f3365fc8cb
ignacio <ignaciojaviercaro@gmail.com>
2020-12-22 00:23:51 -0300

    Segundo commit

--- app/src/main/java/com/example/ejerciciodia3/MainActivity.java
+++ app/src/main/java/com/example/ejerciciodia3/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.ejerciciodia3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        EditText name = findViewById(R.id.name);
+        EditText last_name =findViewById(R.id.lastname);
+        EditText email = findViewById(R.id.email);
+        EditText pass = findViewById(R.id.pass);
+
+
+
+
+
     }
 }
