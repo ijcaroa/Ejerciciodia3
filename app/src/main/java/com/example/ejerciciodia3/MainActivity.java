a386bf8b88806251d9143ba4cbb47118147e6a12
ignacio <ignaciojaviercaro@gmail.com>
2020-12-22 18:35:53 -0300

    Segundo commit

--- app/src/main/java/com/example/ejerciciodia3/MainActivity.java
+++ app/src/main/java/com/example/ejerciciodia3/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.ejerciciodia3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
+import android.graphics.drawable.DrawableContainer;
 import android.os.Bundle;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
@@ -17,6 +20,8 @@ public class MainActivity extends AppCompatActivity {
         EditText last_name =findViewById(R.id.lastname);
         EditText email = findViewById(R.id.email);
         EditText pass = findViewById(R.id.pass);
+        Button button = findViewById(R.id.button);
+
 
 
 
